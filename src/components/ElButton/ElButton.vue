eba3c0ae2b4a018e9af1f6142dcc93586831cb36
zhaozhijiong <15017681+zhao-zhijiong@user.noreply.gitee.com>
2025-12-29 23:14:00 +0800

    feat: 新增Button组件

---
+++ src/components/ElButton/ElButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button ref="_ref" class="el-button" :class="{
+    [`el-button--${type}`]: type,
+    [`el-button--${size}`]: size,
+    'is-plain': plain,
+    'is-round': round,
+    'is-circle': circle,
+    'is-disabled': disabled,
+  }" :disabled="disabled" :type="nativeType" :autofocus="autoFocus">
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { ELButtonProps } from './types';
+
+defineOptions({
+  name: 'ElButton'
+})
+
+withDefaults(defineProps<ELButtonProps>(), {
+  nativeType: 'button'
+})
+
+const _ref = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: _ref
+})
+</script>
+
+<style></style>
---
+++ src/components/ElButton/types.ts
@@ -0,0 +1,18 @@
+export type ElButtonType = 'primary' | 'success' | 'warning' | 'danger'  | 'info'
+export type ElButtonSize = 'large' | 'small'
+export type NativeType = 'button' | 'submit' | 'reset'
+
+export interface ELButtonProps {
+  type?: ElButtonType
+  size?: ElButtonSize
+  plain?: boolean
+  round?: boolean
+  circle?: boolean
+  disabled?: boolean
+  nativeType?: NativeType
+  autoFocus?: boolean
+}
+
+export interface ElButtonInstance {
+  ref: HTMLButtonElement
+}
